8936d91fcc3df4b341b735e480e486de68d1ce88
Simon Randby <simon.randby@gmail.com>
2017-07-11 22:56:00 +0200

    Clean up som ecode, shaddowification

--- src/spinningCube.js
+++ src/spinningCube.js
@@ -7,59 +7,70 @@
         }
       });
 
-    console.log('Layer gets called');
-    this.lampModel = new THREE.Object3D();
-    console.log('creating loadobject function');
-    var loadObject = function (objPath, material, three_object) {
-      console.log('objLoader');
-      var objLoader = new THREE.OBJLoader();
-      console.log('Loader.loadajax');
-      Loader.loadAjax(objPath, function(text) {
-        console.log('now parsing');
-        var object = objLoader.parse(text);
-        console.log('now traversing');
-        object.traverse(function(child) {
-          if (child instanceof THREE.Mesh) {
-            console.log('found mesh child');
-            child.material = material;
-          }
+      this.lampModel = new THREE.Object3D();
+      var loadObject = function (objPath, material, three_object) {
+        var objLoader = new THREE.OBJLoader();
+        Loader.loadAjax(objPath, function(text) {
+          var object = objLoader.parse(text);
+          object.traverse(function(child) {
+            if (child instanceof THREE.Mesh) {
+              child.material = material;
+              child.material.side = THREE.DoubleSide;
+              child.castShadow = true;
+              child.receiveShadow = true;
+            }
+          });
+          three_object.add(object);
         });
-        console.log('now adding');
-        three_object.add(object);
-      });
-    };
-    
-    console.log('finished creating loadobject should be no syntax errors');
-    console.log('now adding lampmodel using loadobject');
-    loadObject('res/lamp/lamp.obj', 
-               new THREE.MeshBasicMaterial({ color: 0x000fff }),
-               this.lampModel );
-    this.scene.add( this.lampModel );
-    console.log('lampmodel added');
-    var light = new THREE.PointLight(0x333333, 1, 100);
-    console.log('setting lights');
-    light.position.set(50, 50, 50);
-    var anotherLight = new THREE.PointLight(0x00ffff, 1, 20);
-    var pointLightHelper = new THREE.PointLightHelper(anotherLight, 2);
-    anotherLight.position.set(0,0,0);
-    this.scene.add(anotherLight);
-    this.scene.add(light);
-    this.scene.add(pointLightHelper);
-    var spotLight = new THREE.SpotLight(0xff0000, 1, 2, 20, 10);
-    spotLight.position.set(1,0.5,0.5);
-    spotLight.target = this.lampModel;
-    this.scene.add(spotLight);
-    this.camera.position.z = 10;
-    console.log('camera and light probz a ok');
-    this.scene.add(new THREE.PointLightHelper(spotLight, 1));
-      this.cube = new THREE.Mesh(new THREE.BoxGeometry(50, 5, 5),
-                             new THREE.MeshBasicMaterial({ color: 0x000fff }));
-    //  this.scene.add(this.cube);
+      };
+
+      var bestMaterial = new THREE.MeshPhysicalMaterial({ color: 0x000fff, shading: THREE.SmoothShading });
+
+      loadObject('res/lamp/lamp.obj', bestMaterial, this.lampModel );
+      this.scene.add( this.lampModel );
+
+
+      var floorMat = new THREE.MeshStandardMaterial( {
+          roughness: 0.8,
+          color: 0xffffff,
+          metalness: 0.2,
+          bumpScale: 0.0005
+        });
+      var floorCubeGeometry = new THREE.BoxGeometry(200,1,200);
+      var floorGeometry = new THREE.PlaneBufferGeometry( 20, 20 );
+      this.floorCube = new THREE.Mesh(
+        // floorGeometry,
+        floorCubeGeometry,
+
+        floorMat
+        // new THREE.MeshPhysicalMaterial({ color: 0xff0000, shading: THREE.SmoothShading })
+      );
+      this.floorCube.receiveShadow = true;
+      // this.floorCube.x = -Math.PI / 2.0;
+      this.floorCube.position.set(0,-2,0);
+      this.scene.add(this.floorCube);
+
+
+
+      this.anotherLight = new THREE.PointLight(0xffffff, 1, 5, 2);
+      this.anotherLight.position.set(0,0,0);
+      this.anotherLight.physicallyCorrectLights = true;
+      this.anotherLight.castShadow = true;
+      var pointLightHelper = new THREE.PointLightHelper(this.anotherLight, 2);
+      this.scene.add(this.anotherLight);
+
+      // var light = new THREE.SpotLight( 0xffffff );
+      // light.castShadow = true;
+      // light.position.set(0,0,0);
+      // this.scene.add( light );
+
+      this.camera.position.z = 10;
     }
 
     update(frame) {
       super.update(frame);
       this.lampModel.rotation.x = Math.cos(frame / 10);
+      this.floorCube.needsUpdate = true;
       //this.cube.rotation.x = Math.sin(frame / 10);
       //this.cube.rotation.y = Math.cos(frame / 10);
     }
