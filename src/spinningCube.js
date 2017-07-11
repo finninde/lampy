9a53bf3759fc44381aae22eb2b1c842a053fdd89
Finn Inderhaug Holme <finninde@stud.ntnu.no>
2017-07-11 21:58:34 +0200

    Giant everythingcommit

--- src/spinningCube.js
+++ src/spinningCube.js
@@ -5,7 +5,8 @@
         outputs: {
           render: new NIN.TextureOutput()
         }
-      });      
+      });
+
     console.log('Layer gets called');
     this.lampModel = new THREE.Object3D();
     console.log('creating loadobject function');
@@ -13,9 +14,7 @@
       console.log('objLoader');
       var objLoader = new THREE.OBJLoader();
       console.log('Loader.loadajax');
-      debugger;
       Loader.loadAjax(objPath, function(text) {
-        debugger;
         console.log('now parsing');
         var object = objLoader.parse(text);
         console.log('now traversing');
@@ -29,23 +28,33 @@
         three_object.add(object);
       });
     };
+    
     console.log('finished creating loadobject should be no syntax errors');
     console.log('now adding lampmodel using loadobject');
-    loadObject('lamp.obj', 
-               new THREE.MeshStandardMaterial({
-                 color: 0x119999,
-                 side: THREE.DoubleSide,
-                 roughness: 1
-               }),
+    loadObject('res/lamp/lamp.obj', 
+               new THREE.MeshBasicMaterial({ color: 0x000fff }),
                this.lampModel );
     this.scene.add( this.lampModel );
     console.log('lampmodel added');
     var light = new THREE.PointLight(0x333333, 1, 100);
     console.log('setting lights');
     light.position.set(50, 50, 50);
+    var anotherLight = new THREE.PointLight(0x00ffff, 1, 20);
+    var pointLightHelper = new THREE.PointLightHelper(anotherLight, 2);
+    anotherLight.position.set(0,0,0);
+    this.scene.add(anotherLight);
     this.scene.add(light);
-    this.camera.position.z = 100;
+    this.scene.add(pointLightHelper);
+    var spotLight = new THREE.SpotLight(0xff0000, 1, 2, 20, 10);
+    spotLight.position.set(1,0.5,0.5);
+    spotLight.target = this.lampModel;
+    this.scene.add(spotLight);
+    this.camera.position.z = 10;
     console.log('camera and light probz a ok');
+    this.scene.add(new THREE.PointLightHelper(spotLight, 1));
+      this.cube = new THREE.Mesh(new THREE.BoxGeometry(50, 5, 5),
+                             new THREE.MeshBasicMaterial({ color: 0x000fff }));
+    //  this.scene.add(this.cube);
     }
 
     update(frame) {
